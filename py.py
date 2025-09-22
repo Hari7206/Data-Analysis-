74b609086e57d88554d4b66a13d8ee57208fc41a
Hari Thapa <harithapa4654@gmail.com>
2025-09-22 11:58:44 +0530

    voting system

--- py.py
+++ py.py
@@ -1,2 +1,33 @@
 """ print("Hello, World!")"""
-print("sajde ka tola")
+#print("sajde ka tola")
+
+#a = 5
+#b = 7
+
+#print(a + b)
+#a , b = b , a 
+#print("this is A NOW " + str(a))
+
+#userName = input("Enter a name: ")
+#userCity = input("Enter a city: ")
+#userage = input("Enter a age: ")
+
+
+#print("Your Name is :", userName)
+#print("Your city is:", userCity)
+#print("Your age is :", userage)
+#inputfirst = int(input("Enter your first number: "))
+#inputSecond = int(input("Enter your second number: "))
+
+#result = print("Your result is:", inputfirst + inputSecond)
+age = int(input("enter your AGE :"))
+
+if age >= 18:
+    print("you can vote")
+else:
+    print("you can't vote")
+
+
+
+
+
---
+++ votingsystem.py
@@ -0,0 +1,6 @@
+age = int(input("enter your AGE :"))
+
+if age >= 18:
+    print("you can vote")
+else:
+    print("you can't vote")
