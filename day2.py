386d958db2251364f712b38d068356c66fbc3e24
Hari Thapa <harithapa4654@gmail.com>
2025-09-23 10:21:58 +0530

    user even odd

--- day2.py
+++ day2.py
@@ -1,5 +1,5 @@
 
-marketvalue = float(input("Enter the market real value: "))
+'''marketvalue = float(input("Enter the market real value: "))
 discountvalue = float(input("Enter how much discount you want: "))
 
 discount_percent = (discountvalue / marketvalue) * 100
@@ -10,3 +10,29 @@ finalprice = marketvalue - discount_percent
 
 print("Your discount % is:", finalprice)
 
+
+
+
+usermarks = int(input("Enter your marks: "))
+
+if usermarks > 90:
+    print("Marks is A")
+elif usermarks >= 80:
+    print("Marks is A+")
+    
+elif usermarks >= 70:
+    print("Marks is B+")
+elif usermarks >= 60:
+    print("Marks is B")
+elif usermarks >= 50:
+    print("Marks is B+")
+else:
+    print("tu fail hai")'''
+
+
+#odd even
+userage  = int(input("Enter your age: "))
+if userage % 2 == 0:
+    print("age is even")
+else:
+    print("age is odd")
---
+++ evenOdd.py
@@ -0,0 +1,6 @@
+#odd even
+userage  = int(input("Enter your age: "))
+if userage % 2 == 0:
+    print("age is even")
+else:
+    print("age is odd")
